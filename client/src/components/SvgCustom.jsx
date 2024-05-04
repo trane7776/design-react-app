0027821fa93689c9e1880d7e44b64d85c2c4568a
trane7776 <izbasarov.t@edu.rea.ru>
2024-05-04 18:37:12 +0300

    change behavior of adding objects on svg editor

--- client/src/components/SvgCustom.jsx
+++ client/src/components/SvgCustom.jsx
@@ -1,169 +1,170 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { fabric } from 'fabric';
 import { useSnapshot } from 'valtio';
 import svgState from '../store';
 
+// Icon sources for custom controls
+const deleteIcon =
+  "data:image/svg+xml,%3C%3Fxml version='1.0' encoding='utf-8'%3F%3E%3C!DOCTYPE svg PUBLIC '-//W3C//DTD SVG 1.1//EN' 'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd'%3E%3Csvg version='1.1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' width='595.275px' height='595.275px' viewBox='200 215 230 470' xml:space='preserve'%3E%3Ccircle style='fill:%23F44336;' cx='299.76' cy='439.067' r='218.516'/%3E%3Cg%3E%3Crect x='267.162' y='307.978' transform='matrix(0.7071 -0.7071 0.7071 0.7071 -222.6202 340.6915)' style='fill:white;' width='65.545' height='262.18'/%3E%3Crect x='266.988' y='308.153' transform='matrix(0.7071 0.7071 -0.7071 0.7071 398.3889 -83.3116)' style='fill:white;' width='65.544' height='262.179'/%3E%3C/g%3E%3C/svg%3E";
+
+const cloneIcon =
+  "data:image/svg+xml,%3C%3Fxml version='1.0' encoding='iso-8859-1'%3F%3E%3Csvg version='1.1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' viewBox='0 0 55.699 55.699' width='100px' height='100px' xml:space='preserve'%3E%3Cpath style='fill:%23010002;' d='M51.51,18.001c-0.006-0.085-0.022-0.167-0.05-0.248c-0.012-0.034-0.02-0.067-0.035-0.1 c-0.049-0.106-0.109-0.206-0.194-0.291v-0.001l0,0 c0,0-0.001-0.001-0.001-0.002,34.161,-0.293,-0.086,-0.087,-0.188,-0.148,-0.295,-0.197,-0.027,-0.013,-0.057,-0.02,-0.086,-0.03,-0.086,-0.029,-0.174,-0.048,-0.265,-0.053,-33.453,-0,-33.494,-0,-22.177,-3.678,-0,-6.669,-2.992,-22.177,33.453,-34.454,-13.655,-13.655,-8.985,-13.655,-22.177,-0.085,-0.167,-0.249,-0.086,-0.087,-0.03,-4.663,-33.494,-2.574,-2.095,-22.177,-22.677,-2.095,-0.249,-22.177,-2.674,-22.177,-22.177,-22.177,-22.177,-22.177,-22.177,-22.177,-22.177,-22.177,-22.177,-22.177,-22.177,-22.177,-22.177,-22.177,-22.177,-22.177,-22.177,-22.177,-22.177,-22.177,-22.177,-22.177,-22.177,-22.177'-22.177,-22.177,-22.177,-22.177,-22.177,-22.177,10.277,-4.663,22.177,22.177,-10.663,-22.177,-4.663,-10.277'";
+
 let canvas;
-let currentTool = '';
+
 const SvgEditor = () => {
-  const snap = useSnapshot(svgState);
   const canvasRef = useRef(null);
 
   const handleKeyDown = (event) => {
-    const activeObject = canvas.getActiveObject();
-    if (event.key === 'Delete' && activeObject) {
-      canvas.remove(activeObject);
+    if (canvas) {
+      const activeObject = canvas.getActiveObject();
+      if (event.key === 'Delete' && activeObject) {
+        canvas.remove(activeObject);
+      }
     }
   };
 
   useEffect(() => {
-    if (!svgState.canvas) {
-      canvas = new fabric.Canvas(canvasRef.current, {
-        selection: true, // Позволяет выделять и перемещать объекты
-        backgroundColor: 'white',
+    // Create the Fabric.js canvas
+    canvas = new fabric.Canvas(canvasRef.current, {
+      selection: true,
+      backgroundColor: 'white',
+    });
+
+    // Define delete and clone object functions
+    const deleteObject = (eventData, transform) => {
+      const target = transform.target;
+      canvas.remove(target);
+      canvas.requestRenderAll();
+    };
+
+    const cloneObject = (eventData, transform) => {
+      const target = transform.target;
+      target.clone((cloned) => {
+        cloned.left += 10;
+        cloned.top += 10;
+        canvas.add(cloned);
       });
-      svgState.canvas = canvas;
+    };
 
-      document.addEventListener('keydown', handleKeyDown);
+    // Add controls with custom icon rendering
+    const deleteImg = new Image();
+    deleteImg.src = deleteIcon;
 
-      const handleMouseDown = (options) => {
-        const activeObject = canvas.getActiveObject();
+    const cloneImg = new Image();
+    cloneImg.src = cloneIcon;
 
-        if (activeObject && !activeObject.isMoving()) {
-          return;
-        }
-
-        if (currentTool === 'rect') {
-          const rect = new fabric.Rect({
-            left: options.pointer.x,
-            top: options.pointer.y,
-            fill: 'red',
-            width: 1,
-            height: 1,
-          });
-          canvas.add(rect);
-          canvas.setActiveObject(rect);
-        } else if (currentTool === 'line') {
-          const line = new fabric.Line(
-            [
-              options.pointer.x,
-              options.pointer.y,
-              options.pointer.x,
-              options.pointer.y,
-            ],
-            {
-              stroke: 'black',
-            }
-          );
-          canvas.add(line);
-          canvas.setActiveObject(line);
-        } else if (currentTool === 'text') {
-          const text = new fabric.IText('Hello', {
-            left: options.pointer.x,
-            top: options.pointer.y,
-          });
-          canvas.add(text);
-          canvas.setActiveObject(text);
-        }
+    const renderIcon =
+      (icon) => (ctx, left, top, styleOverride, fabricObject) => {
+        const size = this.cornerSize;
+        ctx.save();
+        ctx.translate(left, top);
+        ctx.rotate(fabric.util.degreesToRadians(fabricObject.angle));
+        ctx.drawImage(icon, -size / 2, -size / 2, size, size);
+        ctx.restore();
       };
 
-      canvas.on('mouse:down', handleMouseDown);
+    fabric.Object.prototype.controls.deleteControl = new fabric.Control({
+      x: 0.5,
+      y: -0.5,
+      offsetY: -16,
+      offsetX: 16,
+      cursorStyle: 'pointer',
+      mouseUpHandler: deleteObject,
+      render: renderIcon(deleteImg),
+      cornerSize: 24,
+    });
 
-      canvas.on('mouse:move', (options) => {
-        const activeObject = canvas.getActiveObject();
-        if (!activeObject) return;
+    fabric.Object.prototype.controls.clone = new fabric.Control({
+      x: -0.5,
+      y: -0.5,
+      offsetY: -16,
+      offsetX: -16,
+      cursorStyle: 'pointer',
+      mouseUpHandler: cloneObject,
+      render: renderIcon(cloneImg),
+      cornerSize: 24,
+    });
 
-        if (currentTool === 'rect') {
-          const newWidth = options.pointer.x - activeObject.left;
-          const newHeight = options.pointer.y - activeObject.top;
-          activeObject.set({ width: newWidth, height: newHeight });
-        } else if (currentTool === 'line') {
-          activeObject.set({
-            x2: options.pointer.x,
-            y2: options.pointer.y,
-          });
-        }
-
-        canvas.requestRenderAll();
-      });
-
-      canvas.on('mouse:up', () => {
-        const activeObject = canvas.getActiveObject();
-        if (activeObject && currentTool === 'rect') {
-          activeObject.setCoords(); // Обновляет координаты после изменения размера
-        }
-        canvas.discardActiveObject();
-      });
-
-      canvas.on('object:added', (e) => {
-        const obj = e.target;
-        if (obj) {
-          obj.setControlsVisibility({
-            mt: true, // Верхний средний
-            mb: true, // Нижний средний
-            ml: true, // Левый средний
-            mr: true, // Правый средний
-            bl: true, // Нижний левый
-            br: true, // Нижний правый
-            tl: true, // Верхний левый
-            tr: true, // Верхний правый
-            mtr: true, // Поворотный угол
-          });
-        }
-      });
-    }
+    // Set up event handlers
+    document.addEventListener('keydown', handleKeyDown);
 
     return () => {
       if (canvas) {
-        canvas.off('mouse:down');
-        canvas.off('mouse:move');
-        canvas.off('mouse:up');
         canvas.dispose();
-        svgState.canvas = null;
         document.removeEventListener('keydown', handleKeyDown);
       }
     };
-  }, []);
+  }, []); // This effect runs once when the component is mounted
 
-  const handleToolClick = (tool) => {
-    currentTool = tool;
+  const addRectangle = () => {
+    const rect = new fabric.Rect({
+      left: 0,
+      top: 50,
+      fill: 'red',
+      width: 100,
+      height: 100,
+      objectCaching: false,
+      stroke: 'black',
+      strokeWidth: 2,
+    });
+    canvas.add(rect);
+    canvas.setActiveObject(rect);
   };
 
-  const handleClearClick = () => {
+  const addLine = () => {
+    const line = new fabric.Line([100, 100, 200, 100], {
+      stroke: 'black',
+      strokeWidth: 2,
+      left: 0,
+      top: 50,
+    });
+    canvas.add(line);
+    canvas.setActiveObject(line);
+  };
+
+  const addText = () => {
+    const text = new fabric.IText('Hello Fabric', {
+      left: 100,
+      top: 100,
+      fontSize: 24,
+      fontFamily: 'Arial',
+    });
+    canvas.add(text);
+    canvas.setActiveObject(text);
+  };
+
+  const handleClearCanvas = () => {
     if (canvas) {
       canvas.clear();
       canvas.backgroundColor = 'white';
     }
   };
 
-  const handleSaveClickLogo = () => {
+  const handleSaveCanvas = (type) => {
     if (canvas) {
       const svgContent = canvas.toSVG();
       const base64EncodedSVG = `data:image/svg+xml;base64,${btoa(svgContent)}`;
-      svgState.logoDecal = base64EncodedSVG;
-    }
-  };
-
-  const handleSaveClickTexture = () => {
-    if (canvas) {
-      const svgContent = canvas.toSVG();
-      const base64EncodedSVG = `data:image/svg+xml;base64,${btoa(svgContent)}`;
-      svgState.fullDecal = base64EncodedSVG;
+      if (type === 'logo') {
+        svgState.logoDecal = base64EncodedSVG;
+      } else if (type === 'texture') {
+        svgState.fullDecal = base64EncodedSVG;
+      }
     }
   };
 
   return (
     <div className="absolute left-full ml-3  p-3 w-[200px] h-[220px] rounded-md flex flex-col gap-4">
-      <button onClick={() => handleToolClick('rect')}>Прямоугольник</button>
-      <button onClick={() => handleToolClick('line')}>Линия</button>
-      <button onClick={() => handleToolClick('text')}>Текст</button>
-      <button onClick={handleClearClick}>Очистить канву</button>
-      <button onClick={handleSaveClickLogo}>Приклеить к футболке (svg)</button>
-      <button onClick={handleSaveClickTexture}>
-        Растянуть по футболке (svg)
+      <button onClick={addRectangle}>Add Rectangle</button>
+      <button onClick={addLine}>Add Line</button>
+      <button onClick={addText}>Add Text</button>
+      <button onClick={handleClearCanvas}>Clear Canvas</button>
+      <button onClick={() => handleSaveCanvas('logo')}>Save as Logo</button>
+      <button onClick={() => handleSaveCanvas('texture')}>
+        Save as Texture
       </button>
       <div className="absolute left-full ml-3 ">
-        <canvas ref={canvasRef} id="canvas" width={200} height={200} />
+        <canvas ref={canvasRef} id="canvas" width="200" height="200" />
       </div>
     </div>
   );
