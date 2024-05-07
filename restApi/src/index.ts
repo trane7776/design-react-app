30e156edb9fb8c72541c2c6b53a19906642bf7a8
trane7776 <izbasarov.t@edu.rea.ru>
2024-05-07 15:44:05 +0300

    created rest api for login and register, created controllers for auth and manage users, added middleware for manage current session

---
+++ restApi/src/index.ts
@@ -0,0 +1,35 @@
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import router from './router';
+import * as dotenv from 'dotenv';
+dotenv.config();
+const app = express();
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(process.env.PORT, () => {
+  console.log('Server is running on port', process.env.PORT);
+});
+const MONGO_URL = process.env.DB_CONNECT;
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error', (err: Error) => {
+  console.log(err);
+});
+
+app.use('/', router());
